640e30641b49c79e8f7a0746428edd0cd2c541b6
Martin Preisler <Martin Preisler@localhost>
2011-02-13 15:15:40 +0100

    MOD: Vector3 is now a templated class ADD: Some rudimentary documentation for Vector2<T> and Vector3<T>

---
+++ cegui/include/CEGUIVector.h
@@ -0,0 +1,196 @@
+/***********************************************************************
+	filename: 	CEGUIVector.h
+	created:	13/2/2011
+	author:		Martin Preisler (reworked from code by Paul D Turner)
+	
+	purpose:	Defines interfaces for Vector classes
+*************************************************************************/
+/***************************************************************************
+ *   Copyright (C) 2004 - 2011 Paul D Turner & The CEGUI Development Team
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining
+ *   a copy of this software and associated documentation files (the
+ *   "Software"), to deal in the Software without restriction, including
+ *   without limitation the rights to use, copy, modify, merge, publish,
+ *   distribute, sublicense, and/or sell copies of the Software, and to
+ *   permit persons to whom the Software is furnished to do so, subject to
+ *   the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ ***************************************************************************/
+#ifndef _CEGUIVector_h_
+#define _CEGUIVector_h_
+
+#include "CEGUIBase.h"
+#include "CEGUISize.h"
+
+
+// Start of CEGUI namespace section
+namespace CEGUI
+{
+
+/*!
+\brief
+    Class used as a two dimensional vector (aka a Point)
+
+\par
+    This class is templated now, this allows us to use it as a Vector2 of floats,
+    ints or even UDims without replicating the code all over the place.
+
+\par
+    For a simple Vector2 of floats (what was called Vector2 before), use Vector2<>
+    as the T template parameter defaults to float to save fingertips.
+*/
+template<typename T>
+class Vector2:
+    public AllocatedObject<Vector2<T> >
+{
+public:
+    inline Vector2()
+    {}
+    
+    inline Vector2(const T& x, const T& y):
+        d_x(x),
+        d_y(y)
+    {}
+
+    inline Vector2(const Vector2& v):
+        d_x(v.d_x),
+        d_y(v.d_y)
+    {}
+
+    inline Vector2& operator*=(const Vector2& vec)
+    {
+        d_x *= vec.d_x;
+        d_y *= vec.d_y;
+
+        return *this;
+    }
+
+    inline Vector2& operator/=(const Vector2& vec)
+    {
+        d_x /= vec.d_x;
+        d_y /= vec.d_y;
+
+        return *this;
+    }
+
+    inline Vector2& operator+=(const Vector2& vec)
+    {
+        d_x += vec.d_x;
+        d_y += vec.d_y;
+
+        return *this;
+    }
+
+    inline Vector2& operator-=(const Vector2& vec)
+    {
+        d_x -= vec.d_x;
+        d_y -= vec.d_y;
+
+        return *this;
+    }
+
+    inline Vector2 operator+(const Vector2& vec) const
+    {
+        return Vector2(d_x + vec.d_x, d_y + vec.d_y);
+    }
+
+    inline Vector2 operator-(const Vector2& vec) const
+    {
+        return Vector2(d_x - vec.d_x, d_y - vec.d_y);
+    }
+
+    inline Vector2 operator*(const Vector2& vec) const
+    {
+        return Vector2(d_x * vec.d_x, d_y * vec.d_y);
+    }
+
+	inline Vector2 operator*(float c) const
+    {
+        return Vector2(d_x * c, d_y * c);
+    }
+
+    inline bool operator==(const Vector2& vec) const
+    {
+        return ((d_x == vec.d_x) && (d_y == vec.d_y));
+    }
+
+    inline bool operator!=(const Vector2& vec) const
+    {
+        return !(operator==(vec));
+    }
+
+    T d_x;
+    T d_y;
+};
+
+/*!
+\brief
+    Class used as a three dimensional vector
+
+\par
+    This class is templated now, this allows us to use it as a Vector3 of floats,
+    ints or even UDims without replicating the code all over the place.
+
+\par
+    For a simple Vector3 of floats (what was called Vector3 before), use Vector3<>
+    as the T template parameter defaults to float to save fingertips.
+*/
+template<typename T>
+class CEGUIEXPORT Vector3:
+    public AllocatedObject<Vector3<T> >
+{
+public:
+    inline Vector3()
+    {}
+
+    inline Vector3(const T& x, const T& y, const T& z):
+        d_x(x),
+        d_y(y),
+        d_z(z)
+    {}
+
+    inline Vector3(const Vector3& v):
+        d_x(v.d_x),
+        d_y(v.d_y),
+        d_z(v.d_z)
+    {}
+
+    inline bool operator==(const Vector3& vec) const
+    {
+        return ((d_x == vec.d_x) && (d_y == vec.d_y) && (d_z == vec.d_z));
+    }
+
+    inline bool operator!=(const Vector3& vec) const
+    {
+        return !(operator==(vec));
+    }
+
+	inline Vector3 operator*(float c) const
+	{
+		return Vector3(d_x * c, d_y * c, d_z * c);
+	}
+
+	inline Vector3 operator+(const Vector3& v) const
+	{
+		return Vector3(d_x + v.d_x, d_y + v.d_y, d_z + v.d_z);
+	}
+
+    T d_x;
+    T d_y;
+    T d_z;
+};
+
+} // End of  CEGUI namespace section
+
+#endif	// end of guard _CEGUIVector_h_
---
+++ cegui/src/ScriptingModules/PythonScriptModule/bindings/python_CEGUI.h
@@ -0,0 +1,253 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+
+#include "CEGUI.h"
+#include "falagard/CEGUIFalXMLEnumHelper.h"
+
+namespace CEGUI
+{
+
+class PropertyHelper_wrapper
+{
+public:
+    static float stringToFloat(const String& str)
+    {
+        return PropertyHelper<float>::fromString(str);
+    }
+	static unsigned int stringToUint(const String& str)
+	{
+	   return PropertyHelper<uint>::fromString(str);
+    }
+	static bool stringToBool(const String& str)
+	{
+	   return PropertyHelper<bool>::fromString(str);
+    }
+	static Size tringToSize(const String& str)
+	{
+	   return PropertyHelper<Size>::fromString(str);
+    }
+	static Vector2 stringToVector2(const String& str)
+	{
+	   return PropertyHelper<Vector2>::fromString(str);
+    }
+	static Rect stringToRect(const String& str)
+	{
+	   return PropertyHelper<Rect>::fromString(str);
+    }
+	static const Image*	stringToImage(const String& str)
+	{
+	   return PropertyHelper<Image*>::fromString(str);
+    }
+	static Colour stringToColour(const String& str)
+    {
+	   return PropertyHelper<Colour>::fromString(str);
+    }
+	static ColourRect stringToColourRect(const String& str)
+	{
+	   return PropertyHelper<ColourRect>::fromString(str);
+    }
+	static UDim stringToUDim(const String& str)
+	{
+	   return PropertyHelper<UDim>::fromString(str);
+    }
+	static UVector2 stringToUVector2(const String& str)
+	{
+	   return PropertyHelper<UVector2>::fromString(str);
+    }
+	static URect stringToURect(const String& str)
+	{
+	   return PropertyHelper<URect>::fromString(str);
+    }
+    static UBox stringToUBox(const String& str)
+	{
+	   return PropertyHelper<UBox>::fromString(str);
+    }
+
+	static String floatToString(float val)
+	{
+	   return PropertyHelper<float>::toString(val);
+    }
+	static String uintToString(unsigned int val)
+	{
+	   return PropertyHelper<uint>::toString(val);
+    }
+	static String boolToString(bool val)
+	{
+	   return PropertyHelper<bool>::toString(val);
+    }
+	static String sizeToString(const Size& val)
+	{
+	   return PropertyHelper<Size>::toString(val);
+    }
+	static String vector2ToString(const Vector2& val)
+	{
+	   return PropertyHelper<Vector2>::toString(val);
+    }
+	static String rectToString(const Rect& val)
+	{
+	   return PropertyHelper<Rect>::toString(val);
+    }
+	static String imageToString(const Image* val)
+	{
+	   return PropertyHelper<Image*>::toString(val);
+    }
+	static String colourToString(const Colour& val)
+	{
+	   return PropertyHelper<Colour>::toString(val);
+    }
+	static String colourRectToString(const ColourRect& val)
+	{
+	   return PropertyHelper<ColourRect>::toString(val);
+    }
+	static String udimToString(const UDim& val)
+	{
+	   return PropertyHelper<UDim>::toString(val);
+    }
+	static String uvector2ToString(const UVector2& val)
+	{
+	   return PropertyHelper<UVector2>::toString(val);
+    }
+	static String urectToString(const URect& val)
+	{
+	   return PropertyHelper<URect>::toString(val);
+    }
+    static String uboxToString(const UBox& val)
+	{
+	   return PropertyHelper<UBox>::toString(val);
+    }
+};
+
+}
+
+// First we create a magic namespace to hold all our aliases
+namespace pyplusplus
+{
+	namespace aliases
+	{
+        typedef ::CEGUI::NamedXMLResourceManager< CEGUI::Font, CEGUI::Font_xmlHandler >    NamedXMLResourceManagerFont;
+        typedef ::CEGUI::RefCounted< CEGUI::FormattedRenderedString > RefCountedFormattedRenderedString;
+        typedef ::CEGUI::NamedXMLResourceManager< CEGUI::Imageset, CEGUI::Imageset_xmlHandler >  NamedXMLResourceManagerImageset;
+        typedef ::CEGUI::NamedXMLResourceManager< CEGUI::Scheme, CEGUI::Scheme_xmlHandler >  NamedXMLResourceManagerScheme;
+        
+        typedef std::vector<CEGUI::ListboxItem*
+            CEGUI_VECTOR_ALLOC(CEGUI::ListboxItem*)> ListBoxItemPtrVector;
+        // typedef std::vector<CEGUI::Scheme::AliasMapping> AliasMappingVector;
+        typedef std::vector<CEGUI::PropertyInitialiser
+            CEGUI_VECTOR_ALLOC(CEGUI::PropertyInitialiser)> PropertyInitialiserVector;
+        // typedef std::vector<CEGUI::Scheme::FalagardMapping> FalagardMappingVector; 
+        // typedef std::vector<CEGUI::Scheme::LoadableUIElement> LoadableUIElementVector;
+        // typedef std::vector<CEGUI::Scheme::UIElementFactory> UIElementFactoryVector;
+        // typedef std::vector<CEGUI::Scheme::UIModule> UIModuleVector;
+        
+        typedef ::CEGUI::Event::Connection Connection;
+        typedef ::CEGUI::Event::Subscriber Subscriber;
+        typedef ::CEGUI::EventSet::Iterator EventIterator;
+        
+        typedef ::CEGUI::FontManager::FontIterator FontIterator;
+        typedef ::CEGUI::Imageset::ImageIterator ImageIterator;	
+        typedef ::CEGUI::ImagesetManager::ImagesetIterator ImagesetIterator;
+        typedef ::CEGUI::PropertySet::Iterator PropertyIterator;
+        typedef ::CEGUI::SchemeManager::SchemeIterator SchemeIterator;
+        typedef ::CEGUI::WindowFactoryManager::WindowFactoryIterator WindowFactoryIterator;
+        typedef ::CEGUI::WindowFactoryManager::TypeAliasIterator TypeAliasIterator;
+        typedef ::CEGUI::WindowFactoryManager::FalagardMappingIterator FalagardMappingIterator;
+        typedef ::CEGUI::WindowManager::WindowIterator WindowIterator;
+        
+        typedef std::pair<float, float> StdPairFloatFloat;
+        typedef std::map<std::string, std::string
+            CEGUI_MAP_ALLOC(std::string, std::string)> StringMap;
+        typedef std::vector<std::string
+            CEGUI_VECTOR_ALLOC(std::string)> StringVector;
+        
+        typedef CEGUI::Singleton<CEGUI::AnimationManager> SingletonAnimationManager;   
+        typedef CEGUI::Singleton<CEGUI::Logger> SingletonLogger;
+        typedef CEGUI::Singleton<CEGUI::FontManager> SingletonFontManager;
+        typedef CEGUI::Singleton<CEGUI::GlobalEventSet> SingletonGlobalEventSet;
+        typedef CEGUI::Singleton<CEGUI::ImagesetManager> SingletonImagesetManager;
+        typedef CEGUI::Singleton<CEGUI::MouseCursor> SingletonMouseCursor;
+        typedef CEGUI::Singleton<CEGUI::RenderEffectManager> SingletonRenderEffectManager; 
+        typedef CEGUI::Singleton<CEGUI::SchemeManager> SingletonSchemeManager;
+        typedef CEGUI::Singleton<CEGUI::System> SingletonSystem;
+        typedef CEGUI::Singleton<CEGUI::WidgetLookManager> SingletonWidgetLookManager;
+        typedef CEGUI::Singleton<CEGUI::WindowFactoryManager> SingletonWindowFactoryManager;
+        typedef CEGUI::Singleton<CEGUI::WindowManager> SingletonWindowManager;
+        typedef CEGUI::Singleton<CEGUI::WindowRendererManager> SingletonWindowRendererManager;
+        
+        typedef unsigned int Group;
+        typedef std::map<CEGUI::String, CEGUI::String, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::String)> ResourceGroupMap;
+        typedef CEGUI::RefCounted<CEGUI::BoundSlot> Connection;
+        typedef CEGUI::SubscriberSlot Subscriber;
+        typedef std::multimap<Group, Connection
+            CEGUI_MULTIMAP_ALLOC(Group, Connection)> SlotContainer;
+        typedef std::map<CEGUI::String, CEGUI::Event*, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::String)>	EventMap;
+        typedef std::map<CEGUI::String, CEGUI::Font*, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Font*)> FontRegistry;
+        typedef std::vector<CEGUI::Imageset*
+            CEGUI_VECTOR_ALLOC(CEGUI::Imageset*)> ImagesetVector;
+        typedef std::pair<CEGUI::Window*, bool> WindowStackEntry; 
+        typedef std::vector<WindowStackEntry
+            CEGUI_VECTOR_ALLOC(WindowStackEntry)> WindowStack;
+        typedef std::map<CEGUI::String, CEGUI::Property*, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Property*)> PropertyRegistry;
+        typedef std::map<CEGUI::String, CEGUI::String, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::String)> UserStringMap;
+        typedef std::set<CEGUI::String, CEGUI::String::FastLessCompare> BannedXMLPropertySet;
+        typedef std::vector<CEGUI::String
+            CEGUI_VECTOR_ALLOC(CEGUI::String)> TargetTypeStack;		//!< Type used to implement stack of target type names.
+        typedef std::map<CEGUI::String, CEGUI::Window*, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Window*)> WindowRegistry; //!< Type used to implement registry of Window objects
+        typedef std::vector<CEGUI::Window*
+            CEGUI_VECTOR_ALLOC(CEGUI::Window*)>    WindowVector;   //!< Type to use for a collection of Window pointers.
+        typedef std::vector<CEGUI::Property*
+            CEGUI_VECTOR_ALLOC(CEGUI::Property*)> PropertyList;
+        typedef std::map<CEGUI::String, CEGUI::WindowRendererFactory*, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::WindowRendererFactory*)> WR_Registry;
+        typedef std::map<CEGUI::String, CEGUI::String, CEGUI::String::FastLessCompare
+            CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::String)> AttributeMap;
+	}
+} 
+
+// then we exposed everything needed (and more) to ensure GCCXML makes them visible to Py++
+//
+namespace python_CEGUI
+{
+	namespace details
+	{
+		inline void instantiate()
+		{
+            sizeof ( ::std::pair<float, float> );
+            sizeof ( ::CEGUI::Event::Connection ) ;
+            sizeof ( ::CEGUI::Event::Subscriber) ;
+            
+            sizeof ( ::CEGUI::FontManager::FontIterator);
+            sizeof ( ::CEGUI::Imageset::ImageIterator);	
+            sizeof ( ::CEGUI::ImagesetManager::ImagesetIterator);
+            sizeof ( ::CEGUI::PropertySet::Iterator);
+            sizeof ( ::CEGUI::SchemeManager::SchemeIterator);
+            sizeof ( ::CEGUI::WindowFactoryManager::WindowFactoryIterator);
+            sizeof ( ::CEGUI::WindowFactoryManager::TypeAliasIterator);
+            sizeof ( ::CEGUI::WindowFactoryManager::FalagardMappingIterator);
+            sizeof ( ::CEGUI::WindowManager::WindowIterator);
+            sizeof ( ::CEGUI::EventSet::Iterator );
+            sizeof ( ::CEGUI::String::iterator );
+            
+            sizeof ( ::std::pair<float, float> );
+            
+            // seems that doing an sizeof ( ::CEGUI::EventSet::Iterator ); doesn't expose the class :(
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Imageset*, CEGUI::String::FastLessCompare   CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Imageset*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Property*, CEGUI::String::FastLessCompare   CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Property*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Scheme*, CEGUI::String::FastLessCompare     CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Scheme*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Window*, CEGUI::String::FastLessCompare     CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Window*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::WindowFactoryManager::FalagardWindowMapping, CEGUI::String::FastLessCompare CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::WindowFactoryManager::FalagardWindowMapping) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Event*, CEGUI::String::FastLessCompare      CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Event*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::WindowFactoryManager::AliasTargetStack, CEGUI::String::FastLessCompare CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::WindowFactoryManager::AliasTargetStack) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::WindowFactory*, CEGUI::String::FastLessCompare CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::WindowFactory*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Font*, CEGUI::String::FastLessCompare       CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Font*) > > );
+            sizeof(  CEGUI::ConstBaseIterator<std::map<CEGUI::String, CEGUI::Image, CEGUI::String::FastLessCompare      CEGUI_MAP_ALLOC(CEGUI::String, CEGUI::Image) > > );
+		}
+	}
+}
