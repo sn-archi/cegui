f14c45419722cf4b4f1505f728245b6ed3dcdf2b
Martin Preisler <preisler.m@gmail.com>
2011-08-12 16:57:55 +0200

    MOD: Added "version" attribute to the layout file format
    CEGUI 0.8 layouts are version "4"

---
+++ cegui/src/ScriptingModules/PythonScriptModule/bindings/python_CEGUIOpenGLRenderer.h
@@ -0,0 +1,8 @@
+#include "CEGUIString.h"
+#include "CEGUIGeometryBuffer.h"
+#include "CEGUITexture.h"
+#include "CEGUITextureTarget.h"
+#include "CEGUIRenderingRoot.h"
+
+#include "RendererModules/OpenGL/CEGUIOpenGLRenderer.h"
+#include "RendererModules/OpenGL/CEGUIOpenGLViewportTarget.h"
